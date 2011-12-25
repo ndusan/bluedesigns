4a4db85a4dbaa5d5a8f24c6f26eeb5531fa94d02
Emil Ajduk <emil@ajduk.com>
2011-12-25 17:38:26 +0100

    layout and home

--- app/views/home/indexView.php
+++ app/views/home/indexView.php
@@ -1,3 +1,41 @@
+<div class="banner">
+    <? if (!empty($carouselCollection)): ?>
+        <div id="slides">
+            <div class="slides_container">
+                <? foreach ($carouselCollection as $cc): ?>
+                    <div class="slide">
+                        <img src="<?= DS . 'public' . DS . 'uploads' . DS . 'carousel' . DS . $cc['image_name']; ?>" />
+                        <div class="desc">
+                            <? if (!empty($cc['link'])): ?>
+                                <!-- Link -->
+                                <a href="http://<?= rtrim($cc['link'], 'http://'); ?>" target="_blank">link</a>
+                            <? endif; ?>
+                            <p><?= $cc['text']; ?></p>
+                        </div>
+                    </div>
+                <? endforeach; ?>
+            </div>
+        </div>
+    <? endif; ?>
+</div>
+<div class="homeContent">
+    <div class="homeText">
+        <h1>
+            Welcome<br/>
+            to the fresh<br/>
+            way of thinking
+        </h1>
+        <p>
+            Stand out from the crowd. Be unique! We are devoted to designing eye-catching, innovative presentations, because...it’s all about presentation!
+        </p>
+        <p>
+            Blue Designs specializes in providing unique web and graphic solutions that will set your company apart from the competition.
+        </p>
+        <p>
+            We’ve got the vision, we’ve got the talent. Let us prove it to you!
+        </p>
+    </div>
+     <img src="<?= IMAGE_PATH . 'dummy1.jpg'; ?>" />
+</div>
 
-        <? include_once VIEW_PATH.'home'.DS.'_mainNavigation.php'; ?>
-        
+
--- app/views/layout/defaultLayout.php
+++ app/views/layout/defaultLayout.php
@@ -24,10 +24,50 @@
         <link href='http://fonts.googleapis.com/css?family=Francois+One&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
     </head>
     <body data-controller="<?= $this->_controller; ?>" data-method="<?= $this->_action; ?>">
-            
-    <!-- This is a content that will be included on page inside of this layout -->
-    <? if (file_exists(VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'))
-        include (VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'); ?>
-    
+        <div class="headerW">
+            <div class="header"> 
+                <a class="logo" href="/">
+                    <img src="<?= IMAGE_PATH . 'logo.png'; ?>" />
+                </a>
+                <? include_once VIEW_PATH . 'home' . DS . '_mainNavigation.php'; ?>
+            </div>   
+        </div> 
+        <div class="contentW">
+            <div class="content">
+                <!-- This is a content that will be included on page inside of this layout -->
+                <? if (file_exists(VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'))
+                    include (VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'); ?>
+            </div>
+        </div>
+        <div class="bottomW">
+            <div class="bottom">
+                <div class="quotes">
+                    <div class="quoteText">
+                        <p>
+                            We needed to capitalise on our market leading position in grassroots sports marketing and Mr B & Friends created an excellent solution for taking the Activate UK brand forward. The design concept encapsulates our business philosophy and the emotive nature of our industry in a creative and memorable way.
+                        </p>
+                        <h2>Nikola Kovacevic</h2>
+                        <h4>Naxi Media Group</h4>
+                    </div>
+                    <div class="quoteIcons">
+                        <h2>What out clients had to say:</h2>
+                        <ul class="clientsLogos">
+                            <li><a class="active" href="#"><img src="<?= IMAGE_PATH . 'naxiLogo.png'; ?>" /></a></li>
+                            <li><a href="#"><img src="<?= IMAGE_PATH . 'naxiLogo.png'; ?>" /></a></li>
+                            <li><a href="#"><img src="<?= IMAGE_PATH . 'naxiLogo.png'; ?>" /></a></li>
+                            <li><a href="#"><img src="<?= IMAGE_PATH . 'naxiLogo.png'; ?>" /></a></li>
+                            <li><a href="#"><img src="<?= IMAGE_PATH . 'naxiLogo.png'; ?>" /></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <p>&copy;2012 Blue Designs. All rights reserved.</p>
+            <a href="#" class="fb"></a><a href="#" class="tw"></a><a href="#" class="yt"></a>
+            <a class="logoFooter">
+                <img src="<?= IMAGE_PATH . 'logoFooter.png'; ?>" />
+            </a>
+        </div>
     </body>
 </html>
