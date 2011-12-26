cb7b96474c5cbd5e523a299c81925df7831fc712
Katarina <katarina.djuric.jovanovic@gmail.com>
2011-12-26 20:53:19 +0100

    my changes

--- app/views/home/indexView.php
+++ app/views/home/indexView.php
@@ -1,3 +1,33 @@
-
-        <? include_once VIEW_PATH.'home'.DS.'_mainNavigation.php'; ?>
-        
+<div class="mainCarousel"></div>
+<div class="content">
+    <div class="text_box">
+        <ul>
+            <li><h1 class="violet">Welcome to the fresh way of thinking</h1></li>
+            <li>Stand out from the crowd. Be unique! We are devoted to designing eye-catching, innovative presentations, because...it’s all about presentation! Blue Designs specializes in providing unique web and graphic solutions that will set your company apart from the competition. We’ve got the vision, we’ve got the talent. Let us prove it to you! </li>
+        </ul>
+    </div>
+    <div class="img_box"><img src="public/images/index_image.jpg"></div>
+    <div class="index-latest">
+        <h2 class="violet">Latest projects</h2>
+            <ul>
+               <li>
+                   <a href="#">
+                   <img class="captify" rel="caption<?=$lp['id'];?>" src="/images/24-img_latest_tumb.jpg" width="210" height="120" alt="title" />
+                   <div id="caption<?=$lp['id'];?>"><?=$lp['title'];?></div>
+                    </a>
+                 </li>
+            </ul>
+                 
+    </div>
+    <div class="fb-pages"><?=$html->fb(BASE_PATH);?></div>
+</div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- app/views/layout/defaultLayout.php
+++ app/views/layout/defaultLayout.php
@@ -24,10 +24,59 @@
         <link href='http://fonts.googleapis.com/css?family=Francois+One&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
     </head>
     <body data-controller="<?= $this->_controller; ?>" data-method="<?= $this->_action; ?>">
+        <div class="headerC">
+            <div class="header">
+                    <div class="Hlogo"></div>
+                    <div class="langBar">SR / EN</div>
+                    <div class="menu">
+                        <ul>
+                            <li class="btn_home"><a <?=('index'==$this->_action?'style="background-position: 0px -60px;"': '');?> href="/">HOME</a></li>
+                            <li class="btn_studio"><a <?=('studio'==$this->_action?'style="background-position: -110px -60px;"': '');?> href="/studio">STUDIO</a></li>
+                            <li class="btn_portfolio"><a <?=('portfolio'==$this->_action?'style="background-position: -220px -60px;"': '');?> href="/portfolio">OUR WORK</a></li>
+                            <li class="btn_news"><a <?=('news'==$this->_action?'style="background-position: -330px -60px;"': '');?> href="/news">NEWS</a></li>
+                            <li class="btn_download"><a <?=('download'==$this->_action?'style="background-position: -440px -60px;"': '');?> href="/download">DOWNLOADS</a></li>
+                            <li class="btn_contact"><a <?=('contact'==$this->_action?'style="background-position: -550px -60px;"': '');?> href="/contact">CONTACT</a></li>
+                        </ul>
+                    </div>
+             </div>         
+        </div>
+        <div class="mainC">
+            <!-- This is a content that will be included on page inside of this layout -->
+            <? if (file_exists(VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'))
+        include (VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'); ?>  
+        </div>
+        
+        
+       
+        <div class="quotesC">
+            <div class="quotes">
+                <div class="quotes_text">We needed to capitalise on our market leading position in grassroots sports marketing and Mr B & Friends created an excellent solution for taking the Activate UK brand forward. The design concept encapsulates our business philosophy and the emotive nature of our industry in a creative and memorable way.</div>
+                <div class="quotes_signatures">Nikola Kovačević</div>
+                <div class="quotes_companies">Naxi Media Group</div>
+            </div>
+
+            <div class="quotes_clients" id="jall-quotes">
+                <h3 class="white">What our clients had to say:</h3>
+                    <ul>
+                        <li> <img src="<?= IMAGE_PATH . 'usce-logo-inactive.jpg'; ?>" /></li>
+                        <li> <img src="<?= IMAGE_PATH . 'usce-logo-inactive.jpg'; ?>" /></li>
+                        <li> <img src="<?= IMAGE_PATH . 'usce-logo-inactive.jpg'; ?>" /></li>
+                        <li> <img src="<?= IMAGE_PATH . 'usce-logo-inactive.jpg'; ?>" /></li>
+                       
+                    </ul>
+            </div>
             
-    <!-- This is a content that will be included on page inside of this layout -->
-    <? if (file_exists(VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'))
-        include (VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'); ?>
+          </div>
+           <div class="footerC">  
+            <div class="footer-bg-logo">
+                    <div class="footer-text">2011 Blue Designs. All rights reserved</div>
+                    <div class="socialGroups"><img src="<?= IMAGE_PATH . 'fb.png'; ?>" width="30" height="30" /><img src="<?= IMAGE_PATH . 'tw.png'; ?>" width="30" height="30" /><img src="<?= IMAGE_PATH . 'yt.png'; ?>" width="30" height="30" /></div>
+                </div>
+            
+        </div>
+        
+        
+    
     
     </body>
 </html>
