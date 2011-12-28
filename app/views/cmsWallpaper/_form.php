8a31e9d2a123074ef770dff4421a4890473969f3
Dusan Novakovic <ndusan@gmail.com>
2011-12-28 09:01:56 +0100

    set wallpapers

--- app/views/cmsWallpaper/_form.php
+++ app/views/cmsWallpaper/_form.php
@@ -1,25 +1,62 @@
 <div class="addContent">
-    <form action="/cms/wallpaper/<?= $action; ?>" method="post">
-        <table cellpadding="0" cellspacing="0">
-            <tbody>
-                <tr>
-                    <td><span class="jtooltip" title="Image size 770x280px">Image:</span></td>
-                    <td>
-                        <? if (isset($wallpaper['id']) && !empty($wallpaper['image_name'])): ?>
-                            <input type="file" name="image" value=""/>
-                            <a href="<?= DS . 'public' . DS . 'uploads' . DS . 'wallpaper' . DS . $wallpaper['image_name']; ?>" target="_blank"><?= $wallpaper['image_name']; ?></a>
-                        <? else: ?>
-                            <input type="file" name="image" value="" class="jr"/>
-                        <? endif; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2" align="center">
-                        <input type="hidden" name="wallpaper[id]" value="<?= @$wallpaper['id']; ?>" />
-                        <input type="submit" value="Submit" name="submit" />
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <form action="/cms/wallpaper/<?= $action; ?>" method="post" enctype="multipart/form-data">
+    <table cellpadding="0" cellspacing="0">
+        <tbody>
+            <tr>
+                <td>Group</td>
+                <td>Image</td>
+            </tr>
+            <tr>
+                <td>800x600</td>
+                <td>
+                    <? if (!empty($wallpaper['800x600']['image_name'])): ?>
+                        <a href="<?= DS . 'public' . DS . 'uploads' . DS . 'wallpaper' . DS . $wallpaper['800x600']['image_name']; ?>" target="_blank"><?= $wallpaper['800x600']['image_name']; ?></a>
+                        <a href="<?=DS.'cms'.DS.'wallpaper'.DS.'delete'.DS.$wallpaper['800x600']['download_id'].DS.'image'.DS.$wallpaper['800x600']['id'].'?group=800x600';?>" class="cmsDelete jw"></a>
+                    <? else: ?>
+                        <input type="file" name="image[800x600]" value=""/>
+                    <? endif; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>1024x768</td>
+                <td>
+                    <? if (!empty($wallpaper['1024x768']['image_name'])): ?>
+                        <a href="<?= DS . 'public' . DS . 'uploads' . DS . 'wallpaper' . DS . $wallpaper['1024x768']['image_name']; ?>" target="_blank"><?= $wallpaper['1024x768']['image_name']; ?></a>
+                        <a href="<?=DS.'cms'.DS.'wallpaper'.DS.'delete'.DS.$wallpaper['1024x768']['download_id'].DS.'image'.DS.$wallpaper['1024x768']['id'].'?group=1024x768';?>" class="cmsDelete jw"></a>
+                    <? else: ?>
+                        <input type="file" name="image[1024x768]" value=""/>
+                    <? endif; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>1400x1900</td>
+                <td>
+                    <? if (!empty($wallpaper['1400x1900']['image_name'])): ?>
+                        <a href="<?= DS . 'public' . DS . 'uploads' . DS . 'wallpaper' . DS . $wallpaper['1400x1900']['image_name']; ?>" target="_blank"><?= $wallpaper['1400x1900']['image_name']; ?></a>
+                        <a href="<?=DS.'cms'.DS.'wallpaper'.DS.'delete'.DS.$wallpaper['1400x1900']['download_id'].DS.'image'.DS.$wallpaper['1400x1900']['id'].'?group=1400x1900';?>" class="cmsDelete jw"></a>
+                    <? else: ?>
+                        <input type="file" name="image[1400x1900]" value=""/>
+                    <? endif; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>1600x1200</td>
+                <td>
+                    <? if (!empty($wallpaper['1600x1200']['image_name'])): ?>
+                        <a href="<?= DS . 'public' . DS . 'uploads' . DS . 'wallpaper' . DS . $wallpaper['1600x1200']['image_name']; ?>" target="_blank"><?= $wallpaper['1600x1200']['image_name']; ?></a>
+                        <a href="<?=DS.'cms'.DS.'wallpaper'.DS.'delete'.DS.$wallpaper['1600x1200']['download_id'].DS.'image'.DS.$wallpaper['1600x1200']['id'].'?group=1600x1200';?>" class="cmsDelete jw"></a>
+                    <? else: ?>
+                        <input type="file" name="image[1600x1200]" value=""/>
+                    <? endif; ?>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center">
+                    <input type="submit" value="Submit" name="submit" />
+                    <input type="hidden" name="wallpaper[id]" value="<?= @$wallpaper['id']; ?>" />
+                </td>
+            </tr>
+        </tbody>
+    </table>
     </form>
 </div>
