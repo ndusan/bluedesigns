9c5e543d3aed7d97fc305fd9ef23ec87cec495ca
Dusan Novakovic <ndusan@gmail.com>
2011-12-30 23:57:09 +0100

    set news and pagination

--- app/models/NewsModel.php
+++ app/models/NewsModel.php
@@ -4,8 +4,10 @@ class NewsModel extends Model
 {
     
     private $tableNews = 'news';
+    private $tableNewsLanguage = 'news_language';
+    private $tableLanguage = 'language';
     
-    public function getAllRows($params)
+    public function getNumOfRows($params)
     {
         
         try{
@@ -15,6 +17,32 @@ class NewsModel extends Model
             $stmt->execute();
 
             return $stmt->fetch();
+        }catch(Exception $e){
+            
+            return false;
+        }
+    }
+    
+    public function getResults($params, $limit, $perPage)
+    {
+        
+        try{
+            $query = sprintf("SELECT `n`.`image_name`, `nl`.* FROM %s AS `n`
+                                INNER JOIN %s AS `nl` ON `nl`.`news_id`=`n`.`id`
+                                INNER JOIN %s AS `l` ON `l`.`id`=`nl`.`language_id`
+                                WHERE `l`.`iso_code`=:isoCode
+                                ORDER BY `n`.`id` DESC LIMIT %d, %d", 
+                                $this->tableNews, 
+                                $this->tableNewsLanguage, 
+                                $this->tableLanguage, 
+                                $limit, $perPage);
+            $stmt = $this->dbh->prepare($query);
+            
+            $stmt->bindParam(':isoCode', $params['lang'], PDO::PARAM_STR);
+            
+            $stmt->execute();
+
+            return $stmt->fetchAll();
         }catch(Exception $e){
             
             return false;
