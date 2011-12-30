14f729fbb1e1d707108fd5bb9e0f62d83b104baa
Emil Ajduk <emil@ajduk.com>
2011-12-31 00:39:57 +0100

    contact

--- app/views/contact/indexView.php
+++ app/views/contact/indexView.php
@@ -1,1 +1,37 @@
-#contact
+<div class="contentAll colContact">
+    <h2>Feel free to contact us</h2> 
+    <ul class="contact">
+        <li>
+            <p>Blue Designs</p>
+            <p>
+                Bata Lakina 22,
+                11060 Belgrade,
+                Serbia
+            </p>
+            <p>
+                tel: +381 (11) 27 59 513
+                mob: +381 (64) 26 24 284
+                info@blue-designs.rs
+                www.blue-designs.rs
+            </p>
+        </li>
+        <li>
+            <ul>
+                <form>
+                    <li><span>*</span><input type="text" name="name" value="name" /></li>
+                    <li><span>*</span><input type="text" name="" value="e-mail address" /></li>
+                    <li><span>*</span><input type="text" name="" value="phone" /></li>
+                    <li><input type="text" name="" value="company" /></li>
+                    <li><input type="text" name="" value="country" /></li>
+                    <li><textarea name="" rows="4" cols="20">text field</textarea></li>
+                    <li><input type="submit" value="Send" /></li>
+                </form>
+            </ul>
+        </li>
+        <li class="last">
+            <div class="contactMap">
+                <img src="<?= IMAGE_PATH . 'mapa.jpg'; ?>" />
+            </div>
+        </li>
+    </ul>
+</div>
