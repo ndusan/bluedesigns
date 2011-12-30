2074577ca43017d51bd2bc3167306428ec7369cf
Emil Ajduk <emil@ajduk.com>
2011-12-30 23:07:22 +0100

    news

--- app/views/news/indexView.php
+++ app/views/news/indexView.php
@@ -1,1 +1,59 @@
-#news
+<div class="contentAll colNews">
+    <h2>Whats new</h2> 
+    <ul class="news">
+        <li>
+            <div class="newsImg">
+                <img src="<?= IMAGE_PATH . 'dummy4.jpg'; ?>" />
+            </div>
+            <div class="newsText">
+                <h4>Primer samo jedne fotografije</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div class="newsDate">
+                    <span>06</span>
+                    AUG
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="newsImg">
+                <img src="<?= IMAGE_PATH . 'dummy4.jpg'; ?>" />
+            </div>
+            <div class="newsText">
+                <h4>Primer samo jedne fotografije</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div class="newsDate">
+                    <span>06</span>
+                    AUG
+                </div>
+            </div>
+        </li>
+        <li class="last">
+            <div class="newsImg">
+                <img src="<?= IMAGE_PATH . 'dummy4.jpg'; ?>" />
+            </div>
+            <div class="newsText">
+                <h4>Primer samo jedne fotografije</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div class="newsDate">
+                    <span>06</span>
+                    AUG
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
