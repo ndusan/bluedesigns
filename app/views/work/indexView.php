4dedb00ddef5a2cdec50b2efb1ee3af00e852120
Emil Ajduk <emil@ajduk.com>
2011-12-30 22:10:26 +0100

    work

--- app/views/work/indexView.php
+++ app/views/work/indexView.php
@@ -1,1 +1,35 @@
-#work
+<div class="contentAll colWork">
+    <div class="workSidebar">
+        <h2>Clients we did work for:</h2> 
+        <ul class="clientsList">
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a class="active" href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+            <li><a href="#">Naxi Radio</a></li>
+        </ul>
+    </div>
+    <div class="workContent">
+        <h3>Beoplast (Belgrade, Serbia)</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+</div>
