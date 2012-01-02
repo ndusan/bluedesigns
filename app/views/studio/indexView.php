24e8608edb5048c7b726426bf887fe3b9cbdd6fe
Dusan Novakovic <ndusan@gmail.com>
2012-01-02 21:11:54 +0100

    set quotes

--- app/views/studio/indexView.php
+++ app/views/studio/indexView.php
@@ -1,95 +1,9 @@
-<<<<<<< HEAD
-<script>
-    $(function() {
-        $( "#studio" ).accordion();
-    });
-</script>
-=======
->>>>>>> d65e9af3e87db7c92b934470e5ba96f98c2ffdc2
 <div class="banner">
     <? if (!empty($carouselCollection)): ?>
         <? include_once VIEW_PATH . 'home' . DS . '_carouselMain.php'; ?>
     <? endif; ?>
 </div>
 <div class="contentAll colStudio">
-<<<<<<< HEAD
-    <h2>About Blue Designs</h2>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </p>
-    <p>
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-    </p>    
-    <h2><?= $_t['page.studio.subtitle']; ?></h2>
-    <div id="studio">
-        <h3><span class="off"><?= $_t['page.view-more.link']; ?></span><span class="on"><?= $_t['page.view-less.link']; ?></span>Visual Identity</h3>
-        <div>
-            <span class="img">
-                <img src="<?= IMAGE_PATH . 'dummy5.jpg'; ?>" />
-            </span>
-            <span class="txt">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-                <p>
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-                </p>
-                <p>
-                    <a href="#">view our work</a>
-                </p>
-            </span>
-        </div>
-        <h3><span class="off"><?= $_t['page.view-more.link']; ?></span><span class="on"><?= $_t['page.view-less.link']; ?></span>Visual Identity</h3>
-        <div>
-            <span class="img">
-                <img src="<?= IMAGE_PATH . 'dummy5.jpg'; ?>" />
-            </span>
-            <span class="txt">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-                <p>
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-                </p>
-                <p>
-                    <a href="#">view our work</a>
-                </p>
-            </span>
-        </div>
-        <h3><span class="off"><?= $_t['page.view-more.link']; ?></span><span class="on"><?= $_t['page.view-less.link']; ?></span>Visual Identity</h3>
-        <<div>
-            <span class="img">
-                <img src="<?= IMAGE_PATH . 'dummy5.jpg'; ?>" />
-            </span>
-            <span class="txt">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-                <p>
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-                </p>
-                <p>
-                    <a href="#">view our work</a>
-                </p>
-            </span>
-        </div>
-        <h3><span class="off"><?= $_t['page.view-more.link']; ?></span><span class="on"><?= $_t['page.view-less.link']; ?></span>Visual Identity</h3>
-        <div>
-            <span class="img">
-                <img src="<?= IMAGE_PATH . 'dummy5.jpg'; ?>" />
-            </span>
-            <span class="txt">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-                <p>
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-                </p>
-                <p>
-                    <a href="#">view our work</a>
-                </p>
-            </span>
-=======
     <h2><?=$_t['page.studio.subtitle'];?></h2>
     <? if(!empty($studioCollection)):?>
     <div id="studio">
@@ -106,7 +20,6 @@
         <? endif; ?>
         <div>
             <?=$s['text'];?>
->>>>>>> d65e9af3e87db7c92b934470e5ba96f98c2ffdc2
         </div>
         <? endforeach;?>
     </div>
