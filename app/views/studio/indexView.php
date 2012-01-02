f5420d675379dbf0b51e98f9c890c044475064d2
Emil Ajduk <emil@ajduk.com>
2012-01-02 20:27:19 +0100

    push

--- app/views/studio/indexView.php
+++ app/views/studio/indexView.php
@@ -1,8 +1,8 @@
 <script>
-	$(function() {
-		$( "#studio" ).accordion();
-	});
-	</script>
+    $(function() {
+        $( "#studio" ).accordion();
+    });
+</script>
 <div class="banner">
     <? if (!empty($carouselCollection)): ?>
         <div id="slides">
@@ -24,23 +24,82 @@
     <? endif; ?>
 </div>
 <div class="contentAll colStudio">
-    <h2><?=$_t['page.studio.subtitle'];?></h2>
+    <h2>About Blue Designs</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+    <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+    </p>    
+    <h2><?= $_t['page.studio.subtitle']; ?></h2>
     <div id="studio">
-        <h3><span class="off"><?=$_t['page.view-more.link'];?></span><span class="on"><?=$_t['page.view-less.link'];?></span>Visual Identity</h3>
+        <h3><span class="off"><?= $_t['page.view-more.link']; ?></span><span class="on"><?= $_t['page.view-less.link']; ?></span>Visual Identity</h3>
         <div>
-            <p>This is the answer to question #1.  Pellentesque habitant morbi....</p>
+            <span class="img">
+                <img src="<?= IMAGE_PATH . 'dummy5.jpg'; ?>" />
+            </span>
+            <span class="txt">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                </p>
+                <p>
+                    <a href="#">view our work</a>
+                </p>
+            </span>
         </div>
-          <h3><span class="off"><?=$_t['page.view-more.link'];?></span><span class="on"><?=$_t['page.view-less.link'];?></span>Visual Identity</h3>
+        <h3><span class="off"><?= $_t['page.view-more.link']; ?></span><span class="on"><?= $_t['page.view-less.link']; ?></span>Visual Identity</h3>
         <div>
-            <p>This is the answer to question #2.  Pellentesque habitant morbi....</p>
+            <span class="img">
+                <img src="<?= IMAGE_PATH . 'dummy5.jpg'; ?>" />
+            </span>
+            <span class="txt">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                </p>
+                <p>
+                    <a href="#">view our work</a>
+                </p>
+            </span>
         </div>
-          <h3><span class="off"><?=$_t['page.view-more.link'];?></span><span class="on"><?=$_t['page.view-less.link'];?></span>Visual Identity</h3>
-        <div>
-            <p>This is the answer to question #2.  Pellentesque habitant morbi....</p>
+        <h3><span class="off"><?= $_t['page.view-more.link']; ?></span><span class="on"><?= $_t['page.view-less.link']; ?></span>Visual Identity</h3>
+        <<div>
+            <span class="img">
+                <img src="<?= IMAGE_PATH . 'dummy5.jpg'; ?>" />
+            </span>
+            <span class="txt">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                </p>
+                <p>
+                    <a href="#">view our work</a>
+                </p>
+            </span>
         </div>
-          <h3><span class="off"><?=$_t['page.view-more.link'];?></span><span class="on"><?=$_t['page.view-less.link'];?></span>Visual Identity</h3>
+        <h3><span class="off"><?= $_t['page.view-more.link']; ?></span><span class="on"><?= $_t['page.view-less.link']; ?></span>Visual Identity</h3>
         <div>
-            <p>This is the answer to question #2.  Pellentesque habitant morbi....</p>
+            <span class="img">
+                <img src="<?= IMAGE_PATH . 'dummy5.jpg'; ?>" />
+            </span>
+            <span class="txt">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                </p>
+                <p>
+                    <a href="#">view our work</a>
+                </p>
+            </span>
         </div>
     </div>
     <span>fecebook plugin</span>
