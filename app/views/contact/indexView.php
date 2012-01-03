24c80146fcebc2de30d5c7042d6514d2ad7051cc
Emil Ajduk <emil@ajduk.com>
2012-01-03 23:17:59 +0100

    css

--- app/views/contact/indexView.php
+++ app/views/contact/indexView.php
@@ -4,44 +4,47 @@
     <? endif; ?>
 </div>
 <div class="contentAll colContact">
-    <h2><?=$_t['page.contact.title'];?></h2> 
+    <h2><?= $_t['page.contact.title']; ?></h2> 
     <ul class="contact">
         <li>
-            <?=$contactCollection['text'];?>
+            <?= $contactCollection['text']; ?>
         </li>
         <li>
+            <? if (!empty($sent)): ?>
+                <div class="msg success">
+                    <p><?= $_t['contact.sent.label']; ?></p>
+                </div>
+            <? endif; ?>
             <ul>
-                <? if(!empty($sent)):?>
-                <?=$_t['contact.sent.label'];?>
-                <? endif;?>
-                <form action="<?=DS.$params['lang'].DS.'contact';?>" method="post">
+
+                <form action="<?= DS . $params['lang'] . DS . 'contact'; ?>" method="post">
                     <li>
                         <span>*</span>
-                        <input type="text" name="contact[name]" value="<?=$_t['contact.name.label'];?>" title="<?=$_t['contact.name.label'];?>" class="jr form" />
+                        <input type="text" name="contact[name]" value="<?= $_t['contact.name.label']; ?>" title="<?= $_t['contact.name.label']; ?>" class="jr form" />
                     </li>
                     <li>
                         <span>*</span>
-                        <input type="text" name="contact[email]" value="<?=$_t['contact.email.label'];?>" title="<?=$_t['contact.email.label'];?>" class="jr form" />
+                        <input type="text" name="contact[email]" value="<?= $_t['contact.email.label']; ?>" title="<?= $_t['contact.email.label']; ?>" class="jr form" />
                     </li>
                     <li>
                         <span>*</span>
-                        <input type="text" name="contact[phone]" value="<?=$_t['contact.phone.label'];?>" title="<?=$_t['contact.phone.label'];?>" class="jr form" />
+                        <input type="text" name="contact[phone]" value="<?= $_t['contact.phone.label']; ?>" title="<?= $_t['contact.phone.label']; ?>" class="jr form" />
                     </li>
-                    <li><input type="text" name="contact[company]" value="<?=$_t['contact.company.label'];?>" title="<?=$_t['contact.company.label'];?>" class="form"/></li>
-                    <li><input type="text" name="contact[country]" value="<?=$_t['contact.country.label'];?>" title="<?=$_t['contact.country.label'];?>" class="form"/></li>
-                    <li><textarea name="contact[message]" rows="4" cols="20" class="form" title="<?=$_t['contact.text.label'];?>"><?=$_t['contact.text.label'];?></textarea></li>
+                    <li><input type="text" name="contact[company]" value="<?= $_t['contact.company.label']; ?>" title="<?= $_t['contact.company.label']; ?>" class="form"/></li>
+                    <li><input type="text" name="contact[country]" value="<?= $_t['contact.country.label']; ?>" title="<?= $_t['contact.country.label']; ?>" class="form"/></li>
+                    <li><textarea name="contact[message]" rows="4" cols="20" class="form" title="<?= $_t['contact.text.label']; ?>"><?= $_t['contact.text.label']; ?></textarea></li>
                     <li>
-                        <input type="submit" name="submit" value="<?=$_t['contact.sendbtn.label'];?>" />
+                        <input type="submit" name="submit" value="<?= $_t['contact.sendbtn.label']; ?>" />
                     </li>
                 </form>
             </ul>
         </li>
         <li class="last">
-            <? if(!empty($contactCollection['link'])):?>
-            <div class="contactMap">
-                <?=$contactCollection['link'];?>
-            </div>
-            <? endif;?>
+            <? if (!empty($contactCollection['link'])): ?>
+                <div class="contactMap">
+                    <?= $contactCollection['link']; ?>
+                </div>
+            <? endif; ?>
         </li>
     </ul>
     <span><?= $html->fb($_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]); ?></span>
