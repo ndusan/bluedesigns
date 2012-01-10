1b05c3f4fb1ac6d34bf9297983183c358c3181ac
Emil Ajduk <emil@ajduk.com>
2012-01-10 19:06:19 +0100

    css

--- app/views/studio/indexView.php
+++ app/views/studio/indexView.php
@@ -26,7 +26,7 @@
                         <span class="img"><img src="<?= PUBLIC_UPLOAD_PATH . 'studio' . DS . $s['image_name']; ?>" /></span>   
                     <? endif; ?>
                     <span class="txt">
-                        <p><?= $s['text']; ?></p>
+                        <?= $s['text']; ?>
                     </span>
                 </div>
             <? endforeach; ?>
